ea8b560fb465a476a61ae80c9de7482194192b39
Leonardo Benitez <lsbenitezpereira@gmail.com>
2022-05-21 17:59:59 -0300

    basic code for get and post

---
+++ requests-resilient/__init__.py
@@ -0,0 +1,44 @@
+import requests
+from requests import *
+import sys
+import time
+
+def get(**kwargs): #TODO: to support poitional parameters
+    max_retries = 10
+    wait = 1 #seconds
+    
+    retries = 1
+    response = None
+    while True:
+        try:
+            response = requests.get(**kwargs)
+            break
+        except Exception as e:
+            sys.stdout.flush()
+            time.sleep(wait)
+            retries += 1
+            if retries > max_retries:
+                break
+    return response
+
+def post(**kwargs):
+    max_retries = 30
+    wait = 1 #seconds
+    
+    retries = 1
+    response = None
+    while True:
+        try:
+            response = requests.post(**kwargs)
+            break
+        except Exception as e:
+            sys.stdout.flush()
+            time.sleep(wait)
+            retries += 1
+            if retries > max_retries:
+                break
+    return response
+
+
+#r = get('https://google.com')
+#assert r.status_code == 200
